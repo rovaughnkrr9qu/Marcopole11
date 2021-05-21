909921cb8650a884c78d970039b5e591e28bae36
rovaughnkrr9qu <gongping78330308@163.com>
2021-05-21 22:45:55 +0800

    punt B acabat

--- app/src/main/java/sergi/aleix/marc/hommersimpson_marccardenas/MainActivity.java
+++ app/src/main/java/sergi/aleix/marc/hommersimpson_marccardenas/MainActivity.java
@@ -1,13 +1,26 @@
 package sergi.aleix.marc.hommersimpson_marccardenas;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    AnimationDrawable titolSimpsons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView titolAnim =(ImageView)findViewById(R.id.simpsonsAnim);
+        titolAnim.setBackgroundResource(R.drawable.simpsons_anim);
+        titolSimpsons = (AnimationDrawable) titolAnim.getBackground();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean focus) {
+        super.onWindowFocusChanged(focus);
+        titolSimpsons.start();
     }
 }
+
