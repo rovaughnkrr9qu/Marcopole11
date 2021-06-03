2f2eeb52f869e6cae825f3be35dd5fff2faa0393
rovaughnkrr9qu <gongping78330308@163.com>
2021-06-04 07:39:47 +0800

    animacions, sense pulir

--- app/src/main/java/sergi/aleix/marc/hommersimpson_marccardenas/MainActivity.java
+++ app/src/main/java/sergi/aleix/marc/hommersimpson_marccardenas/MainActivity.java
@@ -5,12 +5,15 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     AnimationDrawable titolSimpsons;
     ImageView Hide1, Hide2, Hide3, Hide4, Hide5;
     MediaPlayer audio;
+    private Animation animGirar, animGirarR, animUll, animDonut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         Hide5.setVisibility(View.INVISIBLE);
         audio = MediaPlayer.create(this,R.raw.the_simpsons);
         audio.setLooping(true);
+
+        animGirar= AnimationUtils.loadAnimation(this, R.anim.anim_girar_n);
+        animGirarR = AnimationUtils.loadAnimation(this,R.anim.anim_girar_r);
+        animUll = AnimationUtils.loadAnimation(this,R.anim.anim_ull);
+        animDonut = AnimationUtils.loadAnimation(this, R.anim.anim_donut);
     }
 
     @Override
@@ -45,12 +53,24 @@ public class MainActivity extends AppCompatActivity {
             Hide3.setVisibility(View.INVISIBLE);
             Hide4.setVisibility(View.INVISIBLE);
             Hide5.setVisibility(View.INVISIBLE);
+            Hide1.clearAnimation();
+            Hide2.clearAnimation();
+            Hide3.clearAnimation();
+            Hide3.clearAnimation();
+            Hide4.clearAnimation();
+            Hide5.clearAnimation();
+
         } else {
             Hide1.setVisibility(View.VISIBLE);
             Hide2.setVisibility(View.VISIBLE);
             Hide3.setVisibility(View.VISIBLE);
             Hide4.setVisibility(View.VISIBLE);
             Hide5.setVisibility(View.VISIBLE);
+            Hide1.startAnimation(animGirarR);
+            Hide2.startAnimation(animGirarR);
+            Hide3.startAnimation(animGirar);
+            Hide4.startAnimation(animUll);
+            Hide5.startAnimation(animDonut);
         }
     }
     public void sonarMusic(View view){
